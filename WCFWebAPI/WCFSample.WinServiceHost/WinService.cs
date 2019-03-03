81438831431d0dc21531bcde71210b5ab5f3e7bf
nthdeveloper <mustafakok@gmail.com>
2019-03-03 17:15:57 +0100

    Add Windows service initialization operations.

--- WCFWebAPI/WCFSample.WinServiceHost/WinService.cs
+++ WCFWebAPI/WCFSample.WinServiceHost/WinService.cs
@@ -4,14 +4,18 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using WCFSample.WebAPI;
 
 namespace WCFSample.WinServiceHost
 {
     public partial class WinService : ServiceBase
     {
+        private ServiceHost serviceHost;
+
         public WinService()
         {
             InitializeComponent();
@@ -19,10 +23,23 @@ namespace WCFSample.WinServiceHost
 
         protected override void OnStart(string[] args)
         {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            serviceHost = new ServiceHost(typeof(SampleAPI));
+
+            serviceHost.Open();
         }
 
         protected override void OnStop()
         {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
         }
     }
 }
