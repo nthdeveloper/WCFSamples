a712dc4678cb4003dae05c9a07c45427fcdccc77
nthdeveloper <mustafakok@gmail.com>
2019-03-04 21:33:59 +0100

    Add some more methods. Separate service and contract.

---
+++ WCFWebAPI/WCFSample.WebAPI/ISampleAPI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFSample.WebAPI
+{
+    [ServiceContract(Namespace = "http://www.acme.com/api/")]
+    public interface ISampleAPI
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "Ping")]
+        string Ping();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        Product[] GetAllProducts();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        Product GetProduct(int id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        Product AddProduct(Product product);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        Product UpdateProduct(Product product);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        bool RemoveProduct(int id);
+    }
+
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
--- WCFWebAPI/WCFSample.WebAPI/SampleAPI.cs
+++ WCFWebAPI/WCFSample.WebAPI/SampleAPI.cs
@@ -8,22 +8,74 @@ using System.Web;
 
 namespace WCFSample.WebAPI
 {
-    [ServiceContract(Namespace = "")]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, IncludeExceptionDetailInFaults = true)]
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-    public class SampleAPI
+    public class SampleAPI : ISampleAPI
     {
+        Dictionary<int, Product> products;
+
+        public SampleAPI()
+        {
+            products = new Dictionary<int, Product>()
+            {
+                { 1, new Product(){Id=1, Name="Product 1" }},
+                { 2, new Product(){Id=2, Name="Product 2" }},
+                { 3, new Product(){Id=3, Name="Product 3" }},
+                { 4, new Product(){Id=4, Name="Product 4" }}
+            };
+        }
+
         // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
         // To create an operation that returns XML,
         //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
         //     and include the following line in the operation body:
         //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
-        [OperationContract]
-        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoWork")]
-        public string DoWork()
+        public string Ping()
         {
             return "Hello!";
         }
 
-        // Add more operations here and mark them with [OperationContract]
+        public Product[] GetAllProducts()
+        {
+            return products.Values.ToArray();
+        }
+
+        public Product GetProduct(int id)
+        {
+            if (!products.ContainsKey(id))
+                throw new ArgumentException($"Product with Id {id} does not exist.");
+
+            return products[id];
+        }
+
+        public Product AddProduct(Product product)
+        {
+            if (products.ContainsKey(product.Id))
+                throw new ArgumentException($"Product with Id {product.Id} already exist.");
+
+            products.Add(product.Id, product);
+
+            return product;
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            if (!products.ContainsKey(product.Id))
+                throw new ArgumentException($"Product with Id {product.Id} does not exist.");
+
+            products[product.Id] = product;
+
+            return product;
+        }
+
+        public bool RemoveProduct(int id)
+        {
+            if (!products.ContainsKey(id))
+                throw new ArgumentException($"Product with Id {id} does not exist.");
+
+            products.Remove(id);
+
+            return true;
+        }
     }
 }
